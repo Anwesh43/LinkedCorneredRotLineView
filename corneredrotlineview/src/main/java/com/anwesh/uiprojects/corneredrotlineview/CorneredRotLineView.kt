92f8a8ccd0985c3e8b8348a55cb52941cb620228
Anwesh43 <anweshthecool0@gmail.com>
2020-05-24 04:39:05 +0530

    adding constants and libraries

--- corneredrotlineview/src/main/java/com/anwesh/uiprojects/corneredrotlineview/CorneredRotLineView.kt
+++ corneredrotlineview/src/main/java/com/anwesh/uiprojects/corneredrotlineview/CorneredRotLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.corneredrotlineview
 /**
  * Created by anweshmishra on 24/05/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val upLines : Int = 2
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
