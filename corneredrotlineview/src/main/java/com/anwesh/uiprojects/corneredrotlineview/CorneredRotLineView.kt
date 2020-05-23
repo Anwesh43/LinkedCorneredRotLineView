f8e6e20085e7c505ad6402c32dbf42e35770b012
Anwesh43 <anweshthecool0@gmail.com>
2020-05-24 05:12:15 +0530

    adding CorneredRotLine class

--- corneredrotlineview/src/main/java/com/anwesh/uiprojects/corneredrotlineview/CorneredRotLineView.kt
+++ corneredrotlineview/src/main/java/com/anwesh/uiprojects/corneredrotlineview/CorneredRotLineView.kt
@@ -165,4 +165,28 @@ class CorneredRotLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class CorneredRotLine(var i : Int) {
+
+        private val root : CRLNode = CRLNode(0)
+        private var curr : CRLNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
