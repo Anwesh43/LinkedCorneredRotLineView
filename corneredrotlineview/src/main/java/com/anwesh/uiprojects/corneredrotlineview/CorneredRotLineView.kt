9b8ab8a8f6aa9c075d1e7cd0d800a28bb5c84c6f
Anwesh43 <anweshthecool0@gmail.com>
2020-05-24 04:53:54 +0530

    adding CorneredRotLineView class

--- corneredrotlineview/src/main/java/com/anwesh/uiprojects/corneredrotlineview/CorneredRotLineView.kt
+++ corneredrotlineview/src/main/java/com/anwesh/uiprojects/corneredrotlineview/CorneredRotLineView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawCRLNode(i : Int, scale : Float, paint : Paint) {
     drawCorneredRotLines(scale, size, paint)
     restore()
 }
+
+class CorneredRotLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
